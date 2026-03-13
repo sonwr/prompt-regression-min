a30286b686b19f6e793924a5a56959b5b333dc81
sonwr <wooram.son@gmail.com>
2026-03-13 01:08:25 +0000

    Add semicolon regex flag coverage

---
+++ tests/test_regex_flags_semicolon_string.py
@@ -0,0 +1,6 @@
+from prompt_regression_min.core import _score
+
+
+def test_regex_supports_semicolon_delimited_flag_string() -> None:
+    expected = {"type": "regex", "pattern": r"^alpha.+omega$", "flags": "ignorecase; dotall"}
+    assert _score("ALPHA\nbody\nomega", expected)
